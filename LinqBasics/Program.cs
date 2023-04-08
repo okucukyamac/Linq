18e5cd85838ae790c2f3dfa660fb5d0d5213103c
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-08 15:27:14 +0300

    Add project files.

---
+++ LinqBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LinqBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> countries = new List<string>();
+
+            countries.Add("USA");
+            countries.Add("INDIA");
+            countries.Add("UK");
+
+            foreach (string country in countries)
+            {
+                Console.WriteLine(country);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
