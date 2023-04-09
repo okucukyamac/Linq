4152579bd0d4c7c1939c5b854a94ecd5b516186d
Oğuzhan Küçükyamaç <okucukyamac@gmail.com>
2023-04-09 13:18:09 +0300

    JoinInnerJoin

--- LinqBasics/Methods.cs
+++ LinqBasics/Methods.cs
@@ -8,6 +8,64 @@ namespace LinqBasics
 {
     internal class Methods
     {
+        public static void JoinInnerJoin()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() {Id= 1,Name="Preety",AddressId=1} ,
+                new Employee() {Id= 2,Name="Priyanka",AddressId=2} ,
+                new Employee() {Id= 3,Name="Anurag",AddressId=3} ,
+                new Employee() {Id= 4,Name="Pranaya",AddressId=4} ,
+                new Employee() {Id= 5,Name="Hina",AddressId=12} ,
+                new Employee() {Id= 6,Name="Sambit",AddressId=10} ,
+                new Employee() {Id= 7,Name="Happy",AddressId=6} ,
+                new Employee() {Id= 8,Name="Tarun",AddressId=4} ,
+                new Employee() {Id= 9,Name="Santosh",AddressId=3},
+                new Employee() {Id= 10,Name="Raja",AddressId=2},
+                new Employee() {Id= 11,Name="Sudhanshu",AddressId=7}
+            };
+
+            List<Address> addresses = new List<Address>()
+            {
+                new Address() {Id= 1,AddressLine="AddressLine1"},
+                new Address() {Id= 2,AddressLine="AddressLine2"},
+                new Address() {Id= 3,AddressLine="AddressLine3"},
+                new Address() {Id= 4,AddressLine="AddressLine4"},
+                new Address() {Id= 5,AddressLine="AddressLine5"},
+                new Address() {Id= 6,AddressLine="AddressLine6"},
+                new Address() {Id= 7,AddressLine="AddressLine7"},
+            };
+
+            var result = employees.Join(addresses, emp => emp.AddressId, add => add.Id, (emp, add) => new
+            {
+                EmployeeId = emp.Id,
+                EmployeName = emp.Name,
+                AdressLine = add.AddressLine
+            }).ToList();
+
+            var result2 = from emp in employees
+                          join add in addresses
+                          on emp.AddressId equals add.Id
+                          select new
+                          {
+                              EmployeeId = emp.Id,
+                              EmployeName = emp.Name,
+                              AdressLine = add.AddressLine
+                          };
+
+            foreach (var a in result2)
+            {
+                Console.WriteLine(a.EmployeeId + " - " + a.AdressLine + " - " + a.EmployeName);
+
+            }
+            Console.WriteLine("");
+
+
+            result.ForEach(a =>
+            {
+                Console.WriteLine(a.EmployeeId + " - " + a.AdressLine + " - " + a.EmployeName);
+            });
+        }
 
         public static void GroupBy()
         {
--- LinqBasics/Program.cs
+++ LinqBasics/Program.cs
@@ -15,44 +15,7 @@ namespace LinqBasics
         static void Main(string[] args)
         {
 
-            List<Employee> employees = new List<Employee>()
-            {
-                new Employee() {Id= 1,Name="Preety",AddressId=1} ,
-                new Employee() {Id= 2,Name="Priyanka",AddressId=2} ,
-                new Employee() {Id= 3,Name="Anurag",AddressId=3} ,
-                new Employee() {Id= 4,Name="Pranaya",AddressId=4} ,
-                new Employee() {Id= 5,Name="Hina",AddressId=12} ,
-                new Employee() {Id= 6,Name="Sambit",AddressId=10} ,
-                new Employee() {Id= 7,Name="Happy",AddressId=6} ,
-                new Employee() {Id= 8,Name="Tarun",AddressId=4} ,
-                new Employee() {Id= 9,Name="Santosh",AddressId=3},
-                new Employee() {Id= 10,Name="Raja",AddressId=2},
-                new Employee() {Id= 11,Name="Sudhanshu",AddressId=7}
-            };
-
-            List<Address> addresses = new List<Address>()
-            {
-                new Address() {Id= 1,AddressLine="AddressLine1"},
-                new Address() {Id= 2,AddressLine="AddressLine2"},
-                new Address() {Id= 3,AddressLine="AddressLine3"},
-                new Address() {Id= 4,AddressLine="AddressLine4"},
-                new Address() {Id= 5,AddressLine="AddressLine5"},
-                new Address() {Id= 6,AddressLine="AddressLine6"},
-                new Address() {Id= 7,AddressLine="AddressLine7"},
-            };
-
-            var result = employees.Join(addresses, emp => emp.AddressId, add => add.Id, (emp, add) => new
-            {
-                EmployeeId = emp.Id,
-                EmployeName = emp.Name,
-                AdressLine = add.AddressLine
-            }).ToList();
-
-            result.ForEach(a =>
-            {
-                Console.WriteLine(a.EmployeeId + " - " + a.AdressLine + " - " + a.EmployeName);
-            });
-
+            //Methods.JoinInnerJoin();
             //Methods.GroupBy();
             //Methods.SumMaxMinAverageCountAggregate();
             //Methods.SelectMany();
